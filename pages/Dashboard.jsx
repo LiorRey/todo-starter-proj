9592a9e2bb725bb84d9c71948893dc79a1628bd0
LiorRey <lior_rey@hotmail.com>
2025-09-30 22:21:02 +0300

    Make use of Store and Actions for user and todo

--- pages/Dashboard.jsx
+++ pages/Dashboard.jsx
@@ -1,27 +1,32 @@
+import { Chart } from "../cmps/Chart.jsx"
+import { todoService } from "../services/todo.service.js"
+import { loadTodos } from "../store/actions/todo.actions.js"
+import { showErrorMsg } from "../services/event-bus.service.js"
+
 const { useEffect, useState } = React
-import {Chart} from '../cmps/Chart.jsx'
-import { todoService } from '../services/todo.service.js'
+const { useSelector } = ReactRedux
 
 export function Dashboard() {
+  const todosLength = useSelector(
+    storeState => storeState.todoModule.todos.length
+  )
+  const [importanceStats, setImportanceStats] = useState([])
 
-    const [todos, setTodos] = useState([])
-    const [importanceStats, setImportanceStats] = useState([])
+  useEffect(() => {
+    loadTodos().catch(() => {
+      showErrorMsg("Error occurred while loading todos")
+    })
 
-    useEffect(()=>{
-        todoService.query()
-            .then(setTodos)
-        todoService.getImportanceStats()
-            .then(setImportanceStats)
-    }, [])
+    todoService.getImportanceStats().then(setImportanceStats)
+  }, [])
 
-
-    return (
-        <section className="dashboard">
-            <h1>Dashboard</h1>
-            <h2>Statistics for {todos.length} Todos</h2>
-            <hr />
-            <h4>By Importance</h4>
-            <Chart data={importanceStats}/>
-        </section>
-    )
+  return (
+    <section className="dashboard">
+      <h1>Dashboard</h1>
+      <h2>Statistics for {todosLength} Todos</h2>
+      <hr />
+      <h4>By Importance</h4>
+      <Chart data={importanceStats} />
+    </section>
+  )
 }
--- pages/TodoEdit.jsx
+++ pages/TodoEdit.jsx
@@ -1,76 +1,93 @@
 import { todoService } from "../services/todo.service.js"
+import { saveTodo } from "../store/actions/todo.actions.js"
 import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service.js"
 
 const { useState, useEffect } = React
 const { useNavigate, useParams } = ReactRouterDOM
 
 export function TodoEdit() {
+  const [todoToEdit, setTodoToEdit] = useState(todoService.getEmptyTodo())
+  const navigate = useNavigate()
+  const params = useParams()
 
-    const [todoToEdit, setTodoToEdit] = useState(todoService.getEmptyTodo())
-    const navigate = useNavigate()
-    const params = useParams()
+  useEffect(() => {
+    if (params.todoId) loadTodo()
+  }, [])
 
-    useEffect(() => {
-        if (params.todoId) loadTodo()
-    }, [])
+  function loadTodo() {
+    todoService
+      .get(params.todoId)
+      .then(setTodoToEdit)
+      .catch(err => console.log("err:", err))
+  }
 
-    function loadTodo() {
-        todoService.get(params.todoId)
-            .then(setTodoToEdit)
-            .catch(err => console.log('err:', err))
+  function handleChange({ target }) {
+    const field = target.name
+    let value = target.value
+
+    switch (target.type) {
+      case "number":
+      case "range":
+        value = +value || ""
+        break
+
+      case "checkbox":
+        value = target.checked
+        break
+
+      default:
+        break
     }
 
-    function handleChange({ target }) {
-        const field = target.name
-        let value = target.value
+    setTodoToEdit(prevTodoToEdit => ({ ...prevTodoToEdit, [field]: value }))
+  }
 
-        switch (target.type) {
-            case 'number':
-            case 'range':
-                value = +value || ''
-                break
+  function onSaveTodo(ev) {
+    ev.preventDefault()
+    saveTodo(todoToEdit)
+      .then(() => {
+        showSuccessMsg("Todo saved successfully!")
+        navigate("/todo")
+      })
+      .catch(err => {
+        showErrorMsg("Error occurred while saving a todo")
+      })
+  }
 
-            case 'checkbox':
-                value = target.checked
-                break
+  const { txt, importance, isDone } = todoToEdit
 
-            default:
-                break
-        }
+  return (
+    <section className="todo-edit">
+      <form onSubmit={onSaveTodo}>
+        <label htmlFor="txt">Text:</label>
+        <input
+          onChange={handleChange}
+          value={txt}
+          type="text"
+          name="txt"
+          id="txt"
+        />
 
-        setTodoToEdit(prevTodoToEdit => ({ ...prevTodoToEdit, [field]: value }))
-    }
+        <label htmlFor="importance">Importance:</label>
+        <input
+          onChange={handleChange}
+          value={importance}
+          type="number"
+          name="importance"
+          id="importance"
+        />
 
-    function onSaveTodo(ev) {
-        ev.preventDefault()
-        todoService.save(todoToEdit)
-            .then((savedTodo) => {
-                navigate('/todo')
-                showSuccessMsg(`Todo Saved (id: ${savedTodo._id})`)
-            })
-            .catch(err => {
-                showErrorMsg('Cannot save todo')
-                console.log('err:', err)
-            })
-    }
+        <label htmlFor="isDone">isDone:</label>
+        <input
+          onChange={handleChange}
+          value={isDone}
+          type="checkbox"
+          name="isDone"
+          id="isDone"
+        />
 
-    const { txt, importance, isDone } = todoToEdit
-
-    return (
-        <section className="todo-edit">
-            <form onSubmit={onSaveTodo} >
-                <label htmlFor="txt">Text:</label>
-                <input onChange={handleChange} value={txt} type="text" name="txt" id="txt" />
-
-                <label htmlFor="importance">Importance:</label>
-                <input onChange={handleChange} value={importance} type="number" name="importance" id="importance" />
-
-                <label htmlFor="isDone">isDone:</label>
-                <input onChange={handleChange} value={isDone} type="checkbox" name="isDone" id="isDone" />
-
-
-                <button>Save</button>
-            </form>
-        </section>
-    )
+        <button>Save</button>
+      </form>
+    </section>
+  )
 }
