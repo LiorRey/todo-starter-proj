3836c979807a53bbbb24ad24b13d3706e4547981
LiorRey <lior_rey@hotmail.com>
2025-10-04 18:33:00 +0300

    Add confirmation mechanism before removing a todo

--- cmps/TodoList.jsx
+++ cmps/TodoList.jsx
@@ -1,20 +1,46 @@
 import { TodoPreview } from "./TodoPreview.jsx"
+
+const { useState } = React
 const { Link } = ReactRouterDOM
 
 export function TodoList({ todos, onRemoveTodo, onToggleTodo }) {
+  const [confirmingTodoId, setConfirmingTodoId] = useState(null)
 
-    return (
-        <ul className="todo-list">
-            {todos.map(todo =>
-                <li key={todo._id}>
-                    <TodoPreview todo={todo} onToggleTodo={()=>onToggleTodo(todo)} />
-                    <section>
-                        <button onClick={() => onRemoveTodo(todo._id)}>Remove</button>
-                        <button><Link to={`/todo/${todo._id}`}>Details</Link></button>
-                        <button><Link to={`/todo/edit/${todo._id}`}>Edit</Link></button>
-                    </section>
-                </li>
-            )}
-        </ul>
-    )
+  return (
+    <ul className="todo-list">
+      {todos.map(todo => (
+        <li key={todo._id}>
+          <TodoPreview todo={todo} onToggleTodo={() => onToggleTodo(todo)} />
+          <section className="todo-actions">
+            <button onClick={() => setConfirmingTodoId(todo._id)}>
+              Remove
+            </button>
+            <button>
+              <Link to={`/todo/${todo._id}`}>Details</Link>
+            </button>
+            <button>
+              <Link to={`/todo/edit/${todo._id}`}>Edit</Link>
+            </button>
+
+            <div
+              className={`confirm-remove-container ${
+                confirmingTodoId === todo._id ? "show" : ""
+              }`}
+            >
+              <h4>Remove todo?</h4>
+              <button onClick={() => setConfirmingTodoId(null)}>No</button>
+              <button
+                onClick={() => {
+                  onRemoveTodo(todo._id)
+                  setConfirmingTodoId(null)
+                }}
+              >
+                Yes
+              </button>
+            </div>
+          </section>
+        </li>
+      ))}
+    </ul>
+  )
 }
