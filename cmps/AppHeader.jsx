b0d190ac899b371fb838947807d3e6c60006e01c
LiorRey <lior_rey@hotmail.com>
2025-10-05 08:33:56 +0300

    Add a todos progress bar in AppHeader [Bonus]

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -11,6 +11,7 @@ import { showErrorMsg } from "../services/event-bus.service.js"
 export function AppHeader() {
   const navigate = useNavigate()
   const user = useSelector(storeState => storeState.userModule.loggedInUser)
+  const todos = useSelector(storeState => storeState.todoModule.todos)
 
   function onLogout() {
     logout().catch(err => showErrorMsg("Error occurred during logout"))
@@ -20,6 +21,21 @@ export function AppHeader() {
     setUser(user)
     navigate("/")
   }
+
+  function calculateTodosDonePercentage() {
+    const totalTodos = todos.length
+    if (totalTodos === 0) return 0
+
+    const totalTodosDone = todos.filter(todo => todo.isDone).length
+    const todosDonePercentage = (totalTodosDone / totalTodos) * 100
+
+    if (Number.isInteger(todosDonePercentage)) return todosDonePercentage
+
+    return +todosDonePercentage.toFixed(2)
+  }
+
+  const donePercent = calculateTodosDonePercentage()
+
   return (
     <header className="app-header full main-layout">
       <section className="header-container">
@@ -34,6 +50,19 @@ export function AppHeader() {
             <LoginSignup onSetUser={onSetUser} />
           </section>
         )}
+        <section className="todos-done-progress-container">
+          <div>
+            <label htmlFor="done-progress">Todos done:</label>
+            <h3>{donePercent}%</h3>
+          </div>
+          <progress
+            value={donePercent}
+            max="100"
+            id="done-progress"
+            name="doneProgress"
+          />
+        </section>
+
         <nav className="app-nav">
           <NavLink to="/">Home</NavLink>
           <NavLink to="/about">About</NavLink>
