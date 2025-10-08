1ccbc4c2d05bc227edf728ab57252992ca89975f
LiorRey <lior_rey@hotmail.com>
2025-10-08 22:50:19 +0300

    Fix the bug of images not showing on GitHub Pages

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -3,14 +3,13 @@ import { ToggleButton } from "../cmps/ToggleButton.jsx"
 const { useState } = React
 
 export function Home() {
-    
-    const [isOn, setIsOn] = useState(false)
+  const [isOn, setIsOn] = useState(false)
 
-    return (
-        <section className="home">
-            <h1>Todo's R Us!</h1>
-            <ToggleButton val={isOn} setVal={setIsOn} />
-            {isOn && <img src="../assets/img/todo.png" alt="" />}
-        </section>
-    )
+  return (
+    <section className="home">
+      <h1>Todo's R Us!</h1>
+      <ToggleButton val={isOn} setVal={setIsOn} />
+      {isOn && <img src="assets/img/todo.png" alt="" />}
+    </section>
+  )
 }
